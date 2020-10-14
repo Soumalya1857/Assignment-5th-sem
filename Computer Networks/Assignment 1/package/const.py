689ae3c48416f86f2dce06c56ec9be36913ffcbb
Soumalya1857 <soumalyakundu10@gmail.com>
2020-10-14 20:48:09 +0530

    VRC on sender side working

--- Computer Networks/Assignment 1/package/const.py
+++ Computer Networks/Assignment 1/package/const.py
@@ -1,1 +1,2 @@
-PACKET_SIZE = 8 #8 bits of data will create a packet
+ENCODE_PACKET_SIZE = 1 #8 bits of data will create a packet
+DECODE_PACKET_SIZE = 1
--- Computer Networks/Assignment 1/sender/addRedundancy.py
+++ Computer Networks/Assignment 1/sender/addRedundancy.py
@@ -1,13 +1,47 @@
-def VRC(filename):
-    inFile = open(filename, 'rb')
-    byte = inFile.read(1) # reading 1 byte at a time
-    while byte:
 
+import sys
+sys.path.append('../package')
+import const
+
+
+def VRC(filename):
+    try:
+        inFile = open(filename, 'r')
+    except IOError:
+        sys.exit("No such file exit!")
+
+    outFile = open("/home/soumalya/Desktop/MotherFolder/Assignment-5th-sem/Computer Networks/Assignment 1/VRCOutput.dat","wb")
+    print("Output file created!")
+    byte = inFile.read(const.ENCODE_PACKET_SIZE) # reading 1 byte at a time
+    
+    while byte:
         # implementing even parity method
         # sender append a 1 to make the byte with even no of 1s
 
+        byteAscii = ord(byte) # returns the ascii value of a letter
         count = 0
         for i in range(8):
-            if byte and (1<< i):
+            if byteAscii and (1<< i):
                 count += 1
-        byte = inFile.read(1)
+        byteAscii = byteAscii << 1
+        if count % 2 == 1:
+            byteAscii = byteAscii and 1
+
+        print(byteAscii.bit_length())
+        outFile.write(byteAscii.to_bytes(1,"big"))
+        byte = inFile.read(const.ENCODE_PACKET_SIZE)
+    
+    inFile.close()
+    outFile.close()
+
+    print("VRC added to the text file!")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit("No input file passed!")
+    if len(sys.argv) > 2:
+        sys.exit("Too many arguments passed! ")
+
+    filename = sys.argv[-1]
+    VRC(filename)
