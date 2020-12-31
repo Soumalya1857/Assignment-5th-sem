592d843a7a05166f238e6c39c287091dc7bd94dc
Soumalya1857 <soumalyakundu10@gmail.com>
2020-12-31 21:47:52 +0530

    OS assignment 2 added

---
+++ Operating System/Assignment 2/q4.py
@@ -0,0 +1,18 @@
+import threading
+import time
+import sys
+import os
+from multiprocessing import Process, Pipe
+
+
+
+class Writer:
+
+    pass
+
+class Reader:
+    pass
+
+class Request:
+
+    pass
